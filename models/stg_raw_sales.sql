278a95ab55f15c2f5d1f8fda753adebab2042280
Karla Solis Medina <karlaa.solism@outlook.com>
2024-12-13 12:03:42 +0000

    Models from sources

---
+++ models/stg_raw_sales.sql
@@ -0,0 +1,15 @@
+#stg_raw_sales
+with
+source as (
+    select * from {{source('gz_raw_data', 'raw_gz_sales')}}
+), 
+renamed as (
+    select
+      date_date,
+      orders_id,
+      pdt_id,
+      revenue,
+      quantity
+    from source 
+)
+select * from renamed 
